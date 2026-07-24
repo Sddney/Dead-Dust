fa3b160fe0d74323ff1f3aca5245c323fb2129da
Sydneyglitch <aruzanberikkizi60@gmail.com>
2026-07-24 21:35:28 +0800

    rotation track

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float smoothTime = 0.05f; 
+    [SerializeField] private float smoothTime = 0.05f;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
-    private Vector2 currentVelocity; 
+    private Vector2 currentVelocity;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -25,6 +24,22 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 targetVelocity = moveInput * speed;
-        rb.linearVelocity = Vector2.SmoothDamp(rb.linearVelocity,targetVelocity,ref currentVelocity,smoothTime);
+        rb.linearVelocity = Vector2.SmoothDamp(
+            rb.linearVelocity,
+            targetVelocity,
+            ref currentVelocity,
+            smoothTime);
+
+        RotateTowardsMovement();
+    }
+
+    private void RotateTowardsMovement()
+    {
+        
+        if (moveInput.sqrMagnitude < 0.01f)
+            return;
+
+        float angle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+        rb.rotation = angle - 90f; 
     }
 }
