c1826d24fbbdc81496369735ea483000c7d8fae6
Kirill Golubenko <golubenkokirill@outlook.com>
2026-07-25 01:18:34 +0500

    Add player weapon system and update damage to float

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour, IDamageable
 {
-    public void TakeDamage(int damage)
+    public void TakeDamage(float damage)
     {
         Debug.Log($"{name}:{damage}");
     }
